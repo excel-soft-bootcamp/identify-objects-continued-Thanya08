eabf0a5b5a6eccd0f3374b27dd687837a92cba63
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-20 18:25:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo _instance = new StudentInfo();
+            _instance.SetId("S100");
+            _instance.SetName("tom");
+            _instance.SetMarks(60);
+
+            string idValue = _instance.GetId();
+            string nameValue = _instance.GetName();
+            int marksValue = _instance.GetMarks();
+        }
+    }
+}
